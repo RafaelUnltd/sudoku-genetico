7ac534efee68e61d7842bd55564fce232fe82dc0
RafaelUnltd <rafael.willian.personal@gmail.com>
2023-12-01 11:10:10 -0300

    Adiciona alguns parâmetros e testes

--- neural.py
+++ neural.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import os
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Reshape
 from tensorflow.keras.utils import to_categorical
-
 
 # Load data from CSV
 df = pd.read_csv('neural_test_data/sudoku_m.csv')  # Replace 'your_dataset.csv' with your actual file path
@@ -20,12 +20,17 @@ df['output'] = df['solutions'].apply(string_to_numpy_array)
 df['input_encoded'] = df['input'].apply(lambda x: to_categorical(x, num_classes=10).flatten())
 df['output_encoded'] = df['output']# Encode input
 
-
-
 # Split data into training and testing sets
 train_size = int(0.8 * len(df))
 train_data, test_data = df[:train_size], df[train_size:]
 
+checkpoint_path = "models/sudoku.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
 # Create and compile the model
 model = Sequential()
 model.add(Dense(units=128, input_dim=81 * 10, activation='relu'))
@@ -41,7 +46,8 @@ model.fit(
     np.stack(train_data['output_encoded'].to_numpy()),  # Convert Pandas Series to NumPy array
     epochs=40,
     batch_size=32,
-    validation_split=0.2
+    validation_split=0.2,
+    callbacks=[cp_callback]
 )
 
 # Evaluate the model on the test set
@@ -49,5 +55,16 @@ results = model.evaluate(
     np.stack(test_data['input_encoded'].to_numpy()),  # Convert Pandas Series to NumPy array
     np.stack(test_data['output_encoded'].to_numpy()),  # Convert Pandas Series to NumPy array
 )
+
 print("Test Loss:", results[0])
 print("Test Accuracy:", results[1])
+
+input_sudoku = '000500007064208005730010000000746002980005710020800603002009400500100260619030000'
+input_array = string_to_numpy_array(input_sudoku)
+input_encoded = to_categorical(input_array, num_classes=10).flatten()
+input_encoded = np.expand_dims(input_encoded, axis=0)
+
+a = model.predict(input_encoded)
+a = np.argmax(a, axis=2).flatten()
+a = a.reshape((9, 9))
+print(a)
